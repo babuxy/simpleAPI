2cad13fa33ed94c1b58226ada137034642c35a67
Barnali <babuxy@gmail.com>
2021-12-13 16:33:51 +0530

    Simple API

---
+++ src/main/java/com/example/simpleAPI/SimpleApiApplication.java
@@ -0,0 +1,26 @@
+package com.example.simpleAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@SpringBootApplication
+@RestController
+
+public class SimpleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleApiApplication.class, args);
+
+
+
+		}
+
+
+	}
+
+
